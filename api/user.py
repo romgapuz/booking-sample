1d8b879ded200ab506884031fa12de203c539230
Rom Gapuz <rom.gapuz@gmail.com>
2017-03-17 23:27:31 +0800

    add login endpoint

--- api/user.py
+++ api/user.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from flask.views import MethodView
+from flask import request
 from sqlalchemy.orm.exc import NoResultFound
 from models.user import User
 from schema.user import UserSchema
@@ -12,6 +13,10 @@ def register(app):
     app.add_url_rule(
         '/user/<id>',
         view_func=UserApiId.as_view('user_id')
+    )
+    app.add_url_rule(
+        '/login',
+        view_func=Login.as_view('login')
     )
 
 
@@ -28,3 +33,22 @@ class UserApiId(MethodView):
             return jsonify(UserSchema(many=False).dump(result).data)
         except NoResultFound:
             return jsonify(UserSchema(many=True).dump(None).data), 404
+
+
+class Login(MethodView):
+    def post(self):
+        try:
+            username = request.form['username']
+            password = request.form['password']
+        except Exception:
+            return "Could not validate username and password", 403
+
+        try:
+            user = User.query.filter_by(username=username).one()
+
+            if user.password != password:
+                return "Incorrect password", 403
+        except Exception:
+            return "Username not found", 403
+
+        return "Login successful"
