b1923581bf5ef5db7a05611073178e129d3cd61e
Rom Gapuz <rom.gapuz@gmail.com>
2017-03-18 07:15:12 +0800

    add endpoint for getting workers given a service id

--- api/service.py
+++ api/service.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from models.service import Service
 from models.user import User
 from schema.service import ServiceSchema
+from schema.user import UserSchema
 
 
 def register(app):
@@ -15,6 +16,10 @@ def register(app):
     app.add_url_rule(
         '/user/<id>/service',
         view_func=UserServiceApi.as_view('user_id_service')
+    )
+    app.add_url_rule(
+        '/service/<id>/worker',
+        view_func=ServiceIdWorkerApi.as_view('service_id_worker')
     )
 
 
@@ -33,3 +38,14 @@ class UserServiceApi(MethodView):
             return jsonify(ServiceSchema(many=True).dump(result.services).data)
         except NoResultFound:
             return jsonify(ServiceSchema(many=True).dump([]).data), 404
+
+
+class ServiceIdWorkerApi(MethodView):
+    def get(self, id):
+        try:
+            result = User.query.filter(
+                User.services.any(id=id)
+            ).filter_by(role='Worker').all()
+            return jsonify(UserSchema(many=True).dump(result).data)
+        except NoResultFound:
+            return jsonify(UserSchema(many=True).dump([]).data), 404
