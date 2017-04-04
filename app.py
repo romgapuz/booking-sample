2bd9846ef8a36e80f1363b98e3c417ed1bd2b3ec
Rom Gapuz <rom.gapuz@gmail.com>
2017-04-05 00:02:12 +0800

    revert database config to main app

--- app.py
+++ app.py
@@ -4,14 +4,28 @@ import api.feedback as api_feedback
 import api.service as api_service
 import api.booking as api_booking
 from schema.base import ma
-from models.base import db, config_from_file
+from models.base import db
 import admin
+from utils.config import read_config
+
+# read config
+config = read_config()
 
 # Create application
 app = Flask(__name__)
 
+# Create dummy secrey key so we can use sessions
+app.config['SECRET_KEY'] = config.get('app', 'secret_key')
+
 # configure database
-app = config_from_file(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    config.get('db', 'sqlalchemy_database_uri')
+app.config['SQLALCHEMY_ECHO'] = \
+    True if config.get('db', 'sqlalchemy_echo') == 'True' else False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = \
+    True if \
+    config.get('db', 'sqlalchemy_track_modifications') == 'True' \
+    else False
 db.app = app
 db.init_app(app)
 
